edd07e0adc896ae44ff1301c4adec6f62f444f7a
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-08-21 23:33:29 +0800

    fix(imagestore): support direct OCI directives in Dockerfile reconstruction (#16)

--- internal/imagestore/container_dockerfile_reconstruct_test.go
+++ internal/imagestore/container_dockerfile_reconstruct_test.go
@@ -41,6 +41,37 @@ func TestReconstructDockerfile(t *testing.T) {
 	}
 }
 
+func TestReconstructDockerfile_DirectOCIDirectives(t *testing.T) {
+	configJSON := `{
+		"history": [
+			{"created_by": "USER 1000:1000", "empty_layer": true},
+			{"created_by": "STOPSIGNAL SIGQUIT", "empty_layer": true},
+			{"created_by": "VOLUME /data", "empty_layer": true},
+			{"created_by": "ENTRYPOINT [\"/bin/server\"]", "empty_layer": true}
+		]
+	}`
+
+	instructions, err := ReconstructDockerfile([]byte(configJSON))
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if len(instructions) != 4 {
+		t.Fatalf("expected 4 instructions, got %d", len(instructions))
+	}
+	if instructions[0].Command != "USER" || instructions[0].Arguments != "1000:1000" {
+		t.Errorf("inst[0] = %+v, want USER 1000:1000", instructions[0])
+	}
+	if instructions[1].Command != "STOPSIGNAL" || instructions[1].Arguments != "SIGQUIT" {
+		t.Errorf("inst[1] = %+v, want STOPSIGNAL SIGQUIT", instructions[1])
+	}
+	if instructions[2].Command != "VOLUME" || instructions[2].Arguments != "/data" {
+		t.Errorf("inst[2] = %+v, want VOLUME /data", instructions[2])
+	}
+	if instructions[3].Command != "ENTRYPOINT" || instructions[3].Arguments != "[\"/bin/server\"]" {
+		t.Errorf("inst[3] = %+v, want ENTRYPOINT [\"/bin/server\"]", instructions[3])
+	}
+}
+
 func TestFormatReconstructedDockerfile(t *testing.T) {
 	configJSON := `{"history":[{"created_by":"/bin/sh -c echo hello"}]}`
 	got := FormatReconstructedDockerfile([]byte(configJSON))
