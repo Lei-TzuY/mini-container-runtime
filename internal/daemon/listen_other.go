eaa9a4f0a07f1080acc442613e0c0091c5f35294
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-08-26 06:20:28 +0800

    fix(daemon): enforce local TCP control plane portably

--- internal/daemon/listen_other.go
+++ internal/daemon/listen_other.go
@@ -5,5 +5,10 @@ package daemon
 import "net"
 
 func listen(network, address string) (net.Listener, error) {
+	if network == "tcp" {
+		if err := validateUnauthenticatedTCPAddress(address); err != nil {
+			return nil, err
+		}
+	}
 	return net.Listen(network, address)
 }
