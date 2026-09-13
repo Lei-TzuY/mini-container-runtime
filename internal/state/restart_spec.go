6428c118a36cef113febb38c45e76bf5fb7f31e0
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-14 01:52:03 +0800

    feat(state): persist soft memory limit for restart

--- internal/state/restart_spec.go
+++ internal/state/restart_spec.go
@@ -49,6 +49,7 @@ type RestartSpec struct {
 	ProcessUmaskSet   bool                 `json:"process_umask_set,omitempty"`
 	ProcessUmask      uint32               `json:"process_umask,omitempty"`
 	Memory            int64                `json:"memory,omitempty"`
+	MemoryHigh        int64                `json:"memory_high,omitempty"`
 	CPUWeight         int64                `json:"cpu_weight,omitempty"`
 	CPUs              float64              `json:"cpus,omitempty"`
 	CPUSetCPUs        string               `json:"cpuset_cpus,omitempty"`
